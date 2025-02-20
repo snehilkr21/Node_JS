cdbabe613d68063523d711c4fcc18612aace9714
snehil <snehil@192.168.1.11>
2025-02-20 11:36:55 +0530

    exporting variable with other name

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-const calculateSum = require("./CalculateSum")
+const {calculateSum,temp} = require("./CalculateSum")
 var myName ="snehil"
 let a =10;
 let b=30;
-console.log(myName)
+console.log(myName,temp)
 calculateSum(a,b)
--- CalculateSum.js
+++ CalculateSum.js
@@ -2,4 +2,8 @@ console.log("Calculate Sum Function")
 function calculateSum(a,b){
     console.log(a+b)
 }
-module.exports = calculateSum
+const x=100;
+module.exports = {
+    calculateSum,
+    temp:x
+}
