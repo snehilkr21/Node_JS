395495589a4c6d73e1fb79d450e090afbcc3ae34
snehil <snehil@snehils-MacBook-Air.local>
2025-06-06 02:10:56 +0530

    require the json file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const {calculateSum,calculateMultiply}=require("./Calculation")
+const data = require("./data.json")
 var a=10
 var b=20
 calculateSum(a,b)
 calculateMultiply(a,b)
+console.log(JSON.stringify(data))
 var x=100;
 console.log(x)
