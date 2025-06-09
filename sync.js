5b47cc1a5190e14292ce8e0754f1f24b155a72a8
snehil <snehil@snehils-MacBook-Air.local>
2025-06-09 10:35:34 +0530

    sync program

---
+++ sync.js
@@ -0,0 +1,9 @@
+console.log("sync application started")
+var a=100;
+var b=200;
+function multiplication(a,b){
+    const c=a*b;
+    return c;
+}
+const c=multiplication(a,b)
+console.log("result ",c)
