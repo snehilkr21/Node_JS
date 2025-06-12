14efad72fdde3b3b4a772dacbcb662af51227cd9
snehil <snehil@snehils-MacBook-Air.local>
2025-06-12 14:15:59 +0530

    eventLoop2

---
+++ eventLoop2.js
@@ -0,0 +1,14 @@
+const fs=require("fs")
+const a =100
+setImmediate(()=>console.log("setImmdediate"))
+fs.readFile("./file.txt","utf-8",()=>{
+    console.log("file read")
+})
+setTimeout(()=>console.log("set timeout is called"),0)
+Promise.resolve("Promise").then(console.log)
+process.nextTick(()=>console.log("Process nextTick"))
+function printA(){
+    console.log("a = ",a)
+}
+printA()
+console.log("last line of code")
