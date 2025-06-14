a7b9da42a0d5dcab69a1896e3ee9cd118305f845
snehil <snehil@snehils-MacBook-Air.local>
2025-06-14 18:33:49 +0530

    connecting mongoDB to our local file

---
+++ database.js
@@ -0,0 +1,35 @@
+const URI =
+  "mongodb+srv://snehilkr21:Jo1E33fr7YteWpve@cluster0.ewcfnz0.mongodb.net/";
+
+const { MongoClient } = require("mongodb");
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url =
+  "mongodb+srv://snehilkr21:Jo1E33fr7YteWpve@cluster0.ewcfnz0.mongodb.net/";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "HelloWorld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+
+  // the following code examples can be pasted here...
+
+  //read
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
