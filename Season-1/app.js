844b3d14476ec0653560bc873613472eaa153ddc
snehil <snehil@snehils-MacBook-Air.local>
2025-06-15 20:02:51 +0530

    just chaging value

--- Season-1/app.js
+++ Season-1/app.js
@@ -1,9 +1,9 @@
-const {calculateSum,calculateMultiply}=require("./Calculation")
-const data = require("./data.json")
-var a=10
-var b=200
-calculateSum(a,b)
-calculateMultiply(a,b)
-console.log(JSON.stringify(data))
-var x=100;
-console.log(x)
+const { calculateSum, calculateMultiply } = require("./Calculation");
+const data = require("./data.json");
+var a = 10;
+var b = 2000;
+calculateSum(a, b);
+calculateMultiply(a, b);
+console.log(JSON.stringify(data));
+var x = 100;
+console.log(x);
