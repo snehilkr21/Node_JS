e910f06c66c85594d6c69afa3d9c935f17ca1e56
snehil <snehil@snehils-MacBook-Air.local>
2025-06-15 19:38:00 +0530

    changes

--- Flick-Backend/src/app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const app= express()
-app.use("/test",(req,res)=>{
-   res.send("Hello from server")
-})
-app.listen(7777,()=>{
-    console.log("Server is successfully started")
-})
--- Season-1/app.js
+++ Season-1/app.js
@@ -1,7 +1,7 @@
 const {calculateSum,calculateMultiply}=require("./Calculation")
 const data = require("./data.json")
 var a=10
-var b=20
+var b=200
 calculateSum(a,b)
 calculateMultiply(a,b)
 console.log(JSON.stringify(data))
